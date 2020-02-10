9f81523e59ebf319e703400f9f764ef60e80f1d6
karthik <challakarthik@gmail.com>
2020-02-10 13:46:20 -0500

    update

---
+++ taskmate/todolist_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def todolist(request):
+    context = {
+        "welcome_text":"Welcome to do list page.",
+    }
+    return render(request, 'todolist.html', context)
+
+def contact(request):
+    context = {
+        "welcome_text":"Welcome to contact page.",
+    }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        "welcome_text":"Welcome to about page.",
+    }
+    return render(request, 'about.html', context)
