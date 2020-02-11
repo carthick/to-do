d67c97d15428ea6a9293c1f456feb23c27965ebc
karthik <challakarthik@gmail.com>
2020-02-11 09:32:07 -0500

    update: templates

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -11,12 +11,12 @@ def todolist(request):
 
 def contact(request):
     context = {
-        "contact_text":"Welcome to contact page.",
+        "contact_text":"Redirect to Google maps and type Kailasa you will find me.",
     }
     return render(request, 'contact.html', context)
 
 def about(request):
     context = {
-        "about_text":"Welcome to about page.",
+        "about_text":"I am god ",
     }
     return render(request, 'about.html', context)
