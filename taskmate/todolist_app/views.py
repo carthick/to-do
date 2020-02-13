ebe5e5042c9128584037bae66091de6f3dfe4df4
carthick <challakarthik@gmail.com>
2020-02-13 14:49:54 -0500

    pagination added (#10)
    Paginate

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -15,6 +16,9 @@ def todolist(request):
         return redirect('todolist')
     else:
         all_tasks = TaskList.objects.all()
+        paginator = Paginator(all_tasks, 5)
+        page = request.GET.get('pg')
+        all_tasks = paginator.get_page(page)
         return render(request, 'todolist.html', {'all_tasks':all_tasks})
 
 def contact(request):
