e4d1c6f5bdd17b371af2a9f7de1a69d9c17f5f9e
karthik <challakarthik@gmail.com>
2020-02-13 17:04:49 -0500

    bootstrap changes

--- taskmate/taskmate/urls.py
+++ taskmate/taskmate/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from todolist_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todolist/',include('todolist_app.urls'))
+    path('index', views.index, name='index'),
+    path('todolist/',include('todolist_app.urls')),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 ]
