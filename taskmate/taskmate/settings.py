de62d70142a95eab06321e31d711cb589ea0c214
karthik <challakarthik@gmail.com>
2020-02-17 15:38:30 -0500

    updated STATICFILES_DIRS

--- taskmate/taskmate/settings.py
+++ taskmate/taskmate/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
